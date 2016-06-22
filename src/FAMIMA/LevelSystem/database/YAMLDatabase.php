9f0c8eba5a85620cac6bf07e2ef68e56fb07ed0c
FAMIMA <tokaiha-2015@outlook.jp>
2016-06-22 18:17:49 +0900

    Let`s try!
    動作するように変更...

--- src/FAMIMA/LevelSystem/database/YAMLDatabase.php
+++ src/FAMIMA/LevelSystem/database/YAMLDatabase.php
@@ -25,39 +25,92 @@ class YAMLDatabase implements DatabaseManager
 	}
 
 
-	public function getLevel(string $user)
+		public function getLevel(string $user)
 	{
-
+		if($this->Config->exists($user))
+		{
+			return $this->Config->get($user)["level"];
+		}else{
+			return false;
+		}
 	}
 
 
 	public function setLevel(string $user, int $level)
 	{
-
+		if($this->Config->exists($user))
+		{
+			$cd = $this->Config->get($user);
+			$cd["level"] = $level;
+			$this->Config->set($user, $cd);
+			$this->Config->save();
+		}
 	}
 
 
 	public function getExp(string $user)
 	{
-
+		if($this->Config->exists($user))
+		{
+			return $this->Config->get($user)["exp"];
+		}else{
+			return false;
+		}
 	}
 
 
 	public function setExp(string $user, int $exp)
 	{
+		if($this->Config->exists($user))
+		{
+			$cd = $this->Config->get($user);
+			$cd["exp"] = $exp;
+			$this->Config->set($user, $cd);
+			$this->Config->save();
+		}
+	}
 
+
+	public function getLevelUpExp(string $user)
+	{
+		if($this->Config->exists($user))
+		{
+			return $this->Config->get($user)["levelupexp"];
+		}else{
+			return false;
+		}
+	}
+
+
+	public function setLevelUpExp(string $user, int $exp)
+	{
+		if($this->Config->exists($user))
+		{
+			$cd = $this->Config->get($user);
+			$cd["levelupexp"] = $exp;
+			$this->Config->set($user, $cd);
+			$this->Config->save();
+		}
 	}
 
 
 	public function isRegist(string $user)
 	{
-
+		return $this->Config->exists($user);
 	}
 
 
 	public function registUser(string $user)
 	{
-
+		$data = array(
+			"level" => 1,
+			"exp" => 0,
+			"levelupexp" => 2,
+			"kill" => 0,
+			"death" => 0
+			);
+		$this->Config->set($user, $data);
+		$this->Config->save();
 	}
 
 
