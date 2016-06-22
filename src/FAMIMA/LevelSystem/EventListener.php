c0a6e0c830513dbbf466ccee8eac9a37271a8968
FAMIMA <tokaiha-2015@outlook.jp>
2016-06-22 18:09:59 +0900

    Let`s try!
    一応動くようにするように変更...

--- src/FAMIMA/LevelSystem/EventListener.php
+++ src/FAMIMA/LevelSystem/EventListener.php
@@ -4,8 +4,10 @@ namespace FAMIMA\LevelSystem;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\entity\EntityDamageEvent;
-use pocketmine\event\entity\EntityByEntityDamageEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\utils\TextFormat as TF;
 
 class Eventlistener implements Listener
 {
@@ -20,22 +22,55 @@ class Eventlistener implements Listener
 		$this->ls->getLogger()->info("起動: LevelSyetem.Listener");
 		$this->ls->getServer()->getPluginManager()->registerEvents($this, $this->ls);
 	}
-	
+
+
 	public function onJoin(PlayerJoinEvent $event)
 	{
 		$player = $event->getPlayer();
 		$name = $player->getName();
-		if(!$this->ls->isRegist($name)){
-			$player->sendMessage(TF::BLUE."[LevelSystem]".TF::WHITE."はじめまして, ".$name."さん");
-			$player->sendMessage(TF::BLUE."[LevelSystem]".TF::WHITE.$name."さんのデータを作成しました");
+		if(!$this->ls->isRegist($name))
+		{
+			$player->sendMessage($this->message["ls"]."はじめまして, ".$name."さん");
+			$player->sendMessage($this->message["ls"].$name."さんのデータを作成しました");
 			$this->ls->registUser($name);
 		}else{
-			$player->sendMessage(TF::BLUE."[LevelSystem]".TF::WHITE."ようこそ, ".$name. "さん");
+			$player->sendMessage($this->message["ls"]."ようこそ, ".$name. "さん");
 		}
 	}
 
-	public function onEntityDamage(EntityDamageEvent $event)
+
+	public function onEntityDeath(PlayerDeathEvent $event)
 	{
-		
+		$player = $event->getEntity();
+		$damage = $player->getLastDamageCause();
+		if($damage instanceof EntityDamageByEntityEvent)
+		{
+			$damager = $damage->getDamager();
+			if($damager instanceof Player and $player instanceof Player)
+			{
+				$dmname = $damager->getName();
+				$dename = $player->getName();
+				$dmexp = $this->ls->getExp($dmname);
+				$dmluexp = $this->ls->getLevelUpExp($dmname);
+				$delevel = $this->ls->getLevel($dename);
+				$gexp = $delevel*2 + mt_rand(0, 20);
+				$ndmexp = $dmexp + $gexp;
+				$dmlevel = $this->ls->getLevel($dmname);
+				for($pl = 0; $ndmexp >= $dmluexp; $pl++)
+				{
+					$ndmexp -= $dmluexp;
+					$dmluexp += mt_rand(0, 5) + 5;
+				}
+				$this->ls->addLevel($dmname, $pl);
+				$this->ls->setExp($dmname, $ndmexp);
+				$this->ls->setLevelUpExp($dmname, $dmluexp);
+				$damager->sendMessage($this->message["ls"].$dename."を倒してExpを".$gexp."手に入れた");
+				if($pl > 0)
+				{
+					$level = $dmlevel + $pl;
+					$this->ls->getServer()->broadcastMessage($this->message["ls"].$dmname."がLevelUp! ".$dmlevel."→".$level);
+				}
+			}
+		}
 	}
 }
